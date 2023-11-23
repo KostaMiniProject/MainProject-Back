402ea0aae05128939945368962f654021b1bbe06
ehdrjs0836 <ehdrjs0836@naver.com>
2023-11-23 17:02:43 +0900

    [Feat] 커뮤니티 기능 생성 2일차

---
+++ src/main/java/kosta/main/communityposts/dto/CommunityPostCreateDto.java
@@ -0,0 +1,10 @@
+package kosta.main.communityposts.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CommunityPostCreateDto {
+    private String title;
+    private String content;
+    private String imageUrl;
+}

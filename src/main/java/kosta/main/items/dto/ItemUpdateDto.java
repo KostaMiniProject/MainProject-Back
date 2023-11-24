2d522c09ed486a634e3d2ce43f7dbadb80a7b3f4
oyunojino <dusrn2xlayjoh@gmail.com>
2023-11-24 16:48:44 +0900

    [Feat] 물건 생성 / 수정 DTO 추가

---
+++ src/main/java/kosta/main/items/dto/ItemUpdateDto.java
@@ -0,0 +1,20 @@
+package kosta.main.items.dto;
+
+import jakarta.persistence.*;
+import kosta.main.audit.Auditable;
+import kosta.main.bids.entity.Bid;
+import kosta.main.categories.entity.Category;
+import kosta.main.items.entity.Item;
+import kosta.main.users.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+public class ItemUpdateDto {
+  private String title;
+  private String description;
+  private Item.ItemStatus itemStatus;
+  private String imageUrl;
+}

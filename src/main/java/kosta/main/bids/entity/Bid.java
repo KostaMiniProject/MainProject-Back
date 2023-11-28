c9279607b702671f9c1178ce59ea56c60c5b5252
SCM <hys1693359@gmail.com>
2023-11-28 16:11:44 +0900

    Feat : 커밋 에러 수정

--- src/main/java/kosta/main/bids/entity/Bid.java
+++ src/main/java/kosta/main/bids/entity/Bid.java
@@ -31,7 +31,6 @@ public class Bid extends Auditable {
     @JoinColumn(name = "exchange_post_id")
     private ExchangePost exchangePost;
 
-    @Builder.Default
     @Column(length = 10, nullable = false)
     private BidStatus status = BidStatus.BIDDING;
 

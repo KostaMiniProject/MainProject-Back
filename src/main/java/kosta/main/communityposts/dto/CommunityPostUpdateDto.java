b8751bf2de5678902c87b7af10ccb8ff15fc5f06
ehdrjs0836 <ehdrjs0836@naver.com>
2023-11-30 11:19:43 +0900

    [Feat] 커뮤니티 기능 생성 좋아요 기능

--- src/main/java/kosta/main/communityposts/dto/CommunityPostUpdateDto.java
+++ src/main/java/kosta/main/communityposts/dto/CommunityPostUpdateDto.java
@@ -1,14 +1,9 @@
 package kosta.main.communityposts.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 public class CommunityPostUpdateDto {
     private String title;
     private String content;
-    private String imageUrl;
-
 }
--- src/main/java/kosta/main/communityposts/entity/CommunityPost.java
+++ src/main/java/kosta/main/communityposts/entity/CommunityPost.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import kosta.main.communityposts.dto.CommunityPostUpdateDto;
 import kosta.main.global.audit.Auditable;
 import kosta.main.likes.entity.Like;
-import kosta.main.images.entity.Image;
 import kosta.main.users.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,13 +13,12 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.util.List;
-
 @Entity
 @Table(name = "community_posts")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Builder
 public class CommunityPost extends Auditable {
 
     @Id
@@ -41,24 +39,11 @@ public class CommunityPost extends Auditable {
     private Integer views;
 
     @Column(length = 20)
+    @Builder.Default
     private CommunityPostStatus communityPostStatus = CommunityPostStatus.PUBLIC;
-
-    @OneToMany(mappedBy = "communityPost", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Image> images; // 커뮤니티 게시글의 이미지 리스트
 
     @OneToMany(mappedBy = "communityPost", cascade = CascadeType.REMOVE)
     private List<Like> likePostList = new ArrayList<>();
-
-    private Integer likeCount;
-    @Builder
-    public CommunityPost(Integer communityPostId, User user, String title, String content, String imageUrl) {
-        this.communityPostId = communityPostId;
-        this.user = user;
-        this.title = title;
-        this.content = content;
-        //this.imageUrl = imageUrl;
-        this.likeCount = 0;
-    }
 
     public CommunityPost updateCommunityPost( CommunityPostUpdateDto communityPostUpdateDto) {
         this.content = communityPostUpdateDto.getContent() != null ? communityPostUpdateDto.getContent() : this.content;
@@ -74,13 +59,5 @@ public class CommunityPost extends Auditable {
 
     public void updateCommunityPostStatus(CommunityPost.CommunityPostStatus communityPostStatus) {
         this.communityPostStatus = communityPostStatus;
-    }
-
-    public void likePostUp() {
-        this.likeCount++;
-    }
-
-    public void likePostDown() {
-        this.likeCount--;
     }
 }

3c03ee7079b6c3fc67f4a4e29f5901d37c1170c5
HeWillGoTillTheEnd <kingofunder@gmail.com>
2023-12-01 16:36:37 +0900

    Feat: 로드밸런서 체크용 HealthController 재 추가

---
+++ src/main/java/kosta/main/HealthController.java
@@ -0,0 +1,35 @@
+package kosta.main;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+    //testa
+    @GetMapping()
+    public String healthCheck(){
+        return "health ok!!!";
+    }
+
+    private final Integer serverNumber;
+
+    private HealthController(@Value("${server-number}") final int serverNumber) {
+        this.serverNumber = serverNumber;
+    }
+
+    private int count = 0;
+
+    @GetMapping("/loadcheck")
+    public ResponseEntity<Map<String, Integer>> count() {
+        count ++;
+        Map<String, Integer> data = new HashMap<>();
+        data.put("Server Number", serverNumber);
+        data.put("Visiting Count", count);
+        return ResponseEntity.ok(data);
+    }
+}

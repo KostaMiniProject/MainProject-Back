40f655b74471f4228e69a384bdcb7111e7681dc0
SCM <hys1693359@gmail.com>
2023-12-12 17:03:51 +0900

    Fix : 교환 내역 데이터명 변경

--- src/main/java/kosta/main/exchangehistories/dto/ExchangeHistoriesResponseDTO.java
+++ src/main/java/kosta/main/exchangehistories/dto/ExchangeHistoriesResponseDTO.java
@@ -12,14 +12,14 @@ import java.util.List;
 @AllArgsConstructor
 public class ExchangeHistoriesResponseDTO {
     private LocalDateTime createdAt; // 2023-11-21 화요일 형태로 제공해줘야함
-    private String myName; // 나의 닉네임
-    private String myAddress; // 나의 주소
-    private String myProfileImage; // 나의 프로필 이미지
-    private List<ItemDetailsDTO> myItem; // 내가 건내준 물건의 리스트 (물건 이름, 물건 상세내용, 물건 대표이미지)
-    private String otherUserName; // 상대방 닉네임
-    private String otherUserAddress; // 상대방 사용자의 주소
-    private String otherUserProfileImage; // 상대방 유저의 프로필 이미지
-    private List<ItemHistoryDTO> otherUserItem; // 상대방이 건내준 물건의 리스트 (물건 이름, 물건 상세내용, 물건 대표이미지)
+    private String posterName; // 나의 닉네임
+    private String posterAddress; // 나의 주소
+    private String posterProfileImage; // 나의 프로필 이미지
+    private List<ItemDetailsDTO> posterItem; // 내가 건내준 물건의 리스트 (물건 이름, 물건 상세내용, 물건 대표이미지)
+    private String bidderName; // 상대방 닉네임
+    private String bidderAddress; // 상대방 사용자의 주소
+    private String bidderProfileImage; // 상대방 유저의 프로필 이미지
+    private List<ItemHistoryDTO> bidderItem; // 상대방이 건내준 물건의 리스트 (물건 이름, 물건 상세내용, 물건 대표이미지)
     @Getter
     @AllArgsConstructor
     public static class ItemDetailsDTO {

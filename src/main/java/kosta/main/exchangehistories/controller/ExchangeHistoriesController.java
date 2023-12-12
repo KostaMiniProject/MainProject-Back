0041d8f9b878489cb90894f86dcee1523d386519
SCM <hys1693359@gmail.com>
2023-12-12 10:11:30 +0900

    Fix : 채팅목록 조회시 응답데이터에 chatRoomId 포함

--- src/main/java/kosta/main/exchangehistories/controller/ExchangeHistoriesController.java
+++ src/main/java/kosta/main/exchangehistories/controller/ExchangeHistoriesController.java
@@ -1,22 +1,35 @@
 package kosta.main.exchangehistories.controller;
 
-import kosta.main.users.entity.LoginUser;
-import kosta.main.users.entity.User;
+import kosta.main.exchangehistories.dto.ExchangeHistoriesResponseDto;
+import kosta.main.exchangehistories.service.ExchangeHistoriesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users/histories")
 @RequiredArgsConstructor
 public class ExchangeHistoriesController {
 
-//  @GetMapping("/{exchangeHistoryId}")
-//  public ResponseEntity<?> getAllExchangePostsHistories(@PathVariable("exchangeHistoryId") Integer exexchangeHistoryId,
-//                                                        @LoginUser User user){
-//    return new ResponseEntity(new )
+  private final ExchangeHistoriesService exchangeHistoriesService;
+  // 교환 내역 생성
+//  @PostMapping
+//  public ResponseEntity<?> createExchangeHistory(@RequestBody ExchangeHistoryCreateDto createDto) {
+//    exchangeHistoriesService.createExchangeHistory(createDto);
+//    return ResponseEntity.ok().build();
+//  }
+//
+//  // 교환 내역 조회
+//  @GetMapping("/{id}")
+//  public ResponseEntity<ExchangeHistoriesResponseDto> getExchangeHistory(@PathVariable Integer id) {
+//    ExchangeHistoriesResponseDto responseDto = exchangeHistoriesService.getExchangeHistory(id);
+//    return ResponseEntity.ok(responseDto);
+//  }
+//
+//  // 교환 내역 삭제
+//  @DeleteMapping("/{id}")
+//  public ResponseEntity<?> deleteExchangeHistory(@PathVariable Integer id) {
+//    exchangeHistoriesService.deleteExchangeHistory(id);
+//    return ResponseEntity.ok().build();
 //  }
 }
--- src/main/java/kosta/main/exchangehistories/repository/ExchangeHistoriesRepository.java
+++ src/main/java/kosta/main/exchangehistories/repository/ExchangeHistoriesRepository.java
@@ -1,4 +1,10 @@
 package kosta.main.exchangehistories.repository;
 
-public interface ExchangeHistoriesRepository {
+import kosta.main.exchangehistories.entity.ExchangeHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExchangeHistoriesRepository extends JpaRepository<ExchangeHistory, Integer> {
 }
+

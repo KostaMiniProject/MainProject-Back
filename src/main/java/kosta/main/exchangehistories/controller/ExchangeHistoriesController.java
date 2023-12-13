4c6065dbb4b39f0a9f091a8a86887b7537283805
HeWillGoTillTheEnd <kingofunder@gmail.com>
2023-12-13 12:20:54 +0900

    Fix: 교환 내역 조회 변경

--- src/main/java/kosta/main/exchangehistories/controller/ExchangeHistoriesController.java
+++ src/main/java/kosta/main/exchangehistories/controller/ExchangeHistoriesController.java
@@ -3,9 +3,13 @@ package kosta.main.exchangehistories.controller;
 import kosta.main.exchangehistories.dto.ExchangeHistoryCreateDTO;
 import kosta.main.exchangehistories.dto.ExchangeHistoriesResponseDTO;
 import kosta.main.exchangehistories.service.ExchangeHistoriesService;
+import kosta.main.global.dto.PageInfo;
+import kosta.main.global.dto.PageResponseDto;
 import kosta.main.users.entity.LoginUser;
 import kosta.main.users.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -23,16 +27,18 @@ public class ExchangeHistoriesController {
   private final ExchangeHistoriesService exchangeHistoriesService;
 
   // 교환 내역 생성 = 거래완료후에 실행될 로직 (굳이 컨트롤러에 없어도 될거 같긴합니다.)
-  public ResponseEntity<?> createExchangeHistory(@RequestBody ExchangeHistoryCreateDTO exchangeHistoryCreateDTO, @LoginUser User user) {
-    return new ResponseEntity<>(exchangeHistoriesService.createExchangeHistory(exchangeHistoryCreateDTO, user), HttpStatus.CREATED);
-  }
+//  public ResponseEntity<?> createExchangeHistory(@RequestBody ExchangeHistoryCreateDTO exchangeHistoryCreateDTO, @LoginUser User user) {
+//    return new ResponseEntity<>(exchangeHistoriesService.createExchangeHistory(exchangeHistoryCreateDTO, user), HttpStatus.CREATED);
+//  }
 
   // 교환 내역 조회
   @GetMapping
-  public ResponseEntity<List<ExchangeHistoriesResponseDTO>> getExchangeHistories(
+  public ResponseEntity<?> getExchangeHistories(
       @LoginUser User user,
       @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
-    return ResponseEntity.ok(exchangeHistoriesService.getExchangeHistories(user, pageable));
+    Page<ExchangeHistoriesResponseDTO> exchangeHistories = exchangeHistoriesService.getExchangeHistories(user, pageable);
+    List<ExchangeHistoriesResponseDTO> list = exchangeHistories.stream().toList();
+    return new ResponseEntity<>(new PageResponseDto<>(list, PageInfo.of(exchangeHistories)), HttpStatus.OK);
   }
 
 
--- src/main/java/kosta/main/exchangehistories/service/ExchangeHistoriesService.java
+++ src/main/java/kosta/main/exchangehistories/service/ExchangeHistoriesService.java
@@ -11,9 +11,14 @@ import kosta.main.exchangehistories.entity.ExchangeHistory;
 import kosta.main.exchangehistories.repository.ExchangeHistoriesRepository;
 import kosta.main.exchangeposts.entity.ExchangePost;
 import kosta.main.exchangeposts.repository.ExchangePostsRepository;
+import kosta.main.global.error.exception.BusinessException;
+import kosta.main.global.error.exception.CommonErrorCode;
 import kosta.main.items.entity.Item;
 import kosta.main.users.entity.User;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
@@ -45,7 +50,10 @@ public class ExchangeHistoriesService {
     ExchangePost exchangePost = findEntityById(exchangePostsRepository, exchangeHistoryCreateDTO.getExchangePostId(), "ExchangePost not found");
 
     // 선택된 입찰 정보 가져오기
-    Bid selectedBid = findEntityById(bidRepository, exchangeHistoryCreateDTO.getSelectedBidId(), "Bid not found");
+//    Bid selectedBid = findEntityById(bidRepository, exchangeHistoryCreateDTO.getSelectedBidId(), "Bid not found");
+
+    Bid selectedBid =
+            exchangePost.getBids().stream().filter(bid -> bid.getStatus() == Bid.BidStatus.SELECTED).findFirst().orElseThrow(() -> new BusinessException(CommonErrorCode.SELECTED_BID_NOT_FOUND));
 
     // 게시글 작성자 정보
     User postUser = exchangePost.getUser();
@@ -91,44 +99,52 @@ public class ExchangeHistoriesService {
   }
 
   @Transactional(readOnly = true)
-  public List<ExchangeHistoriesResponseDTO> getExchangeHistories(User user, Pageable pageable) {
-    List<ExchangeHistory> histories = exchangeHistoriesRepository.findByExchangeInitiator_UserIdOrExchangePartner_UserId(user.getUserId(), user.getUserId(), pageable);
+  public Page<ExchangeHistoriesResponseDTO> getExchangeHistories(User user, Pageable pageable) {
+    Page<ExchangeHistory> histories = exchangeHistoriesRepository.findByExchangeInitiator_UserIdOrExchangePartner_UserId(user.getUserId(), user.getUserId(), pageable);
 
+    List<ExchangeHistoriesResponseDTO> exchangeHistoriesResponseDTOS = makeExchangeHistoriesResponseDTO(histories);
+    PageRequest pageRequest = PageRequest.of(histories.getNumber(), histories.getSize());
+    int start = (int) pageRequest.getOffset();
+    int end = Math.min((start + pageRequest.getPageSize()), exchangeHistoriesResponseDTOS.size());
+    return new PageImpl<>(exchangeHistoriesResponseDTOS.subList(start, end), pageRequest, exchangeHistoriesResponseDTOS.size());
+  }
+
+  private static List<ExchangeHistoriesResponseDTO> makeExchangeHistoriesResponseDTO(Page<ExchangeHistory> histories) {
     return histories.stream().map(history -> {
       ExchangePost exchangePost = history.getExchangePost();
       Bid selectedBid = history.getExchangePost().getBids().stream()
-          .filter(bid -> bid.getStatus() == Bid.BidStatus.SELECTED)
-          .findFirst()
-          .orElseThrow(() -> new EntityNotFoundException("Selected bid not found"));
+              .filter(bid -> bid.getStatus() == Bid.BidStatus.SELECTED)
+              .findFirst()
+              .orElseThrow(() -> new EntityNotFoundException("Selected bid not found"));
       List<ItemHistoryDTO> exchangeHistory = history.getExchangedItems();
 
       List<ExchangeHistoriesResponseDTO.ItemDetailsDTO> myItems = Collections.singletonList(
-          new ExchangeHistoriesResponseDTO.ItemDetailsDTO(
-              exchangePost.getItem().getItemId(),
-              exchangePost.getItem().getTitle(),
-              exchangePost.getItem().getDescription(),
-              exchangePost.getItem().getImages().isEmpty() ? null : exchangePost.getItem().getImages().get(0)
-          )
+              new ExchangeHistoriesResponseDTO.ItemDetailsDTO(
+                      exchangePost.getItem().getItemId(),
+                      exchangePost.getItem().getTitle(),
+                      exchangePost.getItem().getDescription(),
+                      exchangePost.getItem().getImages().isEmpty() ? null : exchangePost.getItem().getImages().get(0)
+              )
       );
 
       List<ExchangeHistoriesResponseDTO.ItemHistoryDTO> otherUserItems = exchangeHistory.stream()
-          .map(item -> new ExchangeHistoriesResponseDTO.ItemHistoryDTO(
-              item.getTitle(),
-              item.getDescription(),
-              item.getImageUrl().isEmpty() ? null : Collections.singletonList(item.getImageUrl())
-          ))
-          .collect(Collectors.toList());
+              .map(item -> new ExchangeHistoriesResponseDTO.ItemHistoryDTO(
+                      item.getTitle(),
+                      item.getDescription(),
+                      item.getImageUrl().isEmpty() ? null : Collections.singletonList(item.getImageUrl())
+              ))
+              .collect(Collectors.toList());
 
       return new ExchangeHistoriesResponseDTO(
-          history.getCreatedAt(),
-          exchangePost.getUser().getName(),
-          exchangePost.getUser().getAddress(),
-          exchangePost.getUser().getProfileImage(),
-          myItems,
-          selectedBid.getUser().getName(),
-          selectedBid.getUser().getAddress(),
-          selectedBid.getUser().getProfileImage(),
-          otherUserItems
+              history.getCreatedAt(),
+              exchangePost.getUser().getName(),
+              exchangePost.getUser().getAddress(),
+              exchangePost.getUser().getProfileImage(),
+              myItems,
+              selectedBid.getUser().getName(),
+              selectedBid.getUser().getAddress(),
+              selectedBid.getUser().getProfileImage(),
+              otherUserItems
       );
     }).collect(Collectors.toList());
   }

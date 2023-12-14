d9f69139a489abf2e21122be0ffba39c30fb084b
oyunojino <dusrn2xlayjoh@gmail.com>
2023-12-14 16:40:17 +0900

    [Feat] 아이디 찾기 관련 DTO 추가

---
+++ src/main/java/kosta/main/users/dto/UserFindIdDTO.java
@@ -0,0 +1,13 @@
+package kosta.main.users.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserFindIdDTO {
+//  private Integer userId;
+  private String name;
+  private String phone;
+}

5b0148d8f755da72aa36323d60e9adabded8fd90
oyunojino <dusrn2xlayjoh@gmail.com>
2023-12-14 12:42:58 +0900

    [Feat] 이메일 전송 및 이메일 인증 요청 기능 구현

--- src/main/java/kosta/main/email/controller/EmailController.java
+++ src/main/java/kosta/main/email/controller/EmailController.java
@@ -1,7 +1,8 @@
 package kosta.main.email.controller;
 
 import jakarta.validation.Valid;
-import kosta.main.email.dto.EmailRequestDto;
+import kosta.main.email.dto.EmailCheckDto;
+import kosta.main.email.dto.EmailSendDto;
 import kosta.main.email.service.EmailSendService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,22 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EmailController {
   private final EmailSendService emailService;
-  @PostMapping ("/api/mail-send")
-  public String mailSend(@RequestBody @Valid EmailRequestDto emailDto){
-    System.out.println("##### 이메일 인증 요청 #####");
-    System.out.println("이메일 :"+emailDto.getEmail());
-    return emailService.joinEmail(emailDto.getEmail());
+
+  @PostMapping("/api/mailSend")
+  public String mailSend(@RequestBody @Valid EmailSendDto emailSendDto) {
+    System.out.println("##### 인증번호 이메일 전송 #####");
+    System.out.println("이메일 :" + emailSendDto.getEmail());
+    return emailService.joinEmail(emailSendDto.getEmail());
   }
 
-
-//  @PostMapping("/mailauthCheck")
-//  public String AuthCheck(@RequestBody @Valid EmailCheckDto emailCheckDto){
-//    Boolean Checked=emailService.CheckAuthNum(emailCheckDto.getEmail(),emailCheckDto.getAuthNum());
-//    if(Checked){
-//      return "ok";
-//    }
-//    else{
-//      throw new NullPointerException("뭔가 잘못!");
-//    }
-//  }
+  @PostMapping("/api/authNumCheck")
+  public Boolean authNumCheck(@RequestBody @Valid EmailCheckDto emailCheckDto) {
+    System.out.println("##### 이메일 인증 요청 #####");
+    System.out.println("입력값 : " + emailCheckDto.getEmailCheckNum());
+    return emailService.emailAuthNumCheck(emailCheckDto);
+  }
 }
--- src/main/java/kosta/main/email/service/EmailSendService.java
+++ src/main/java/kosta/main/email/service/EmailSendService.java
@@ -1,5 +1,11 @@
 package kosta.main.email.service;
 
+import kosta.main.email.dto.EmailCheckDto;
+import kosta.main.email.entity.Emails;
+import kosta.main.email.repository.EmailsRepository;
+import kosta.main.global.error.exception.BusinessException;
+import kosta.main.global.error.exception.CommonErrorCode;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -7,44 +13,72 @@ import org.springframework.stereotype.Service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Random;
 
 @Service
+@Transactional
+@AllArgsConstructor
 public class EmailSendService {
+
+  private final EmailsRepository emailsRepository;
+
   @Autowired
   private JavaMailSender javaMailSender;
-  private int authNumber;
 
-//  @Autowired
-//  private RedisUtil redisUtil;
-//  public boolean CheckAuthNum(String email,String authNum){
-//    if(redisUtil.getData(authNum)==null){
-//      return false;
-//    }
-//    else if(redisUtil.getData(authNum).equals(email)){
-//      return true;
-//    }
-//    else{
-//      return false;
-//    }
-//  }
 
-  // # 임의의 6자리 양수 생성
-  public void makeRandomNumber() {
+
+  /**
+   * 이메일 인증
+   * @param emailCheckDto
+   * @return
+   */
+  public Boolean emailAuthNumCheck(EmailCheckDto emailCheckDto) {
+//    4. 인증번호 확인
+    Emails emails = emailsRepository.findById(emailCheckDto.getEmail()).orElseThrow(() -> new BusinessException(CommonErrorCode.EMAIL_NOT_FOUND));
+    boolean result = emails.getAuthNum().toString().equals(emailCheckDto.getEmailCheckNum());
+    if (result) {
+      emailsRepository.deleteById(emailCheckDto.getEmail());
+    }
+    return result;
+  }
+
+
+
+  /**
+   * 임의의 6자리 양수 생성
+   * @return
+   */
+  public Integer makeRandomNumber() {
     Random r = new Random();
     String randomNumber = "";
     for (int i = 0; i < 6; i++) {
       randomNumber += Integer.toString(r.nextInt(10));
     }
 
-    authNumber = Integer.parseInt(randomNumber);
+    return Integer.parseInt(randomNumber);
   }
 
 
-  // # 메일을 보내기 위한 상세 정보
+
+  /**
+   * 메일을 보내기 위한 상세 정보
+   * @param email
+   * @return
+   */
   public String joinEmail(String email) {
-    makeRandomNumber();
+    //    1. 임의의 6자리 수 생성
+    Integer authNumber = makeRandomNumber();
+
+    //    2. 이메일과 임의의 수를 Email 테이블의 저장
+    Emails newEmailCheck = Emails.builder()
+        .email(email)
+        .authNum(authNumber)
+        .build();
+    emailsRepository.save(newEmailCheck);
+
+    //    3. 입력된 메일에 인증 번호 전송
     String setFrom = "itsopshop2023@gmail.com";
     String toMail = email;
     String title = "회원 가입 인증 이메일 입니다.";
@@ -53,12 +87,20 @@ public class EmailSendService {
             "<br><br>" +
             "인증 번호는 <strong>" + authNumber + "</strong> 입니다." +
             "<br>" +
-            "인증번호를 제대로 입력해주세요!!!"; // 이메일 내용 삽입
+            "인증번호를 제대로 입력해주세요!!!";
     mailSend(setFrom, toMail, title, content);
     return Integer.toString(authNumber);
   }
 
-  // # 이메일 전송 과정
+
+
+  /**
+   * 이메일 전송 과정
+   * @param setFrom
+   * @param toMail
+   * @param title
+   * @param content
+   */
   public void mailSend(String setFrom, String toMail, String title, String content) {
     MimeMessage message = javaMailSender.createMimeMessage();//JavaMailSender 객체를 사용하여 MimeMessage 객체를 생성
     try {
@@ -69,13 +111,14 @@ public class EmailSendService {
       helper.setFrom(setFrom);    // 이메일 발신자 주소 설정
       helper.setTo(toMail);       // 이메일 수신자 주소 설정
       helper.setSubject(title);   // 이메일 제목 설정
-      helper.setText(content, true);  // 이메일의 내용 설정, true값을 통해 html로 지정
+      helper.setText(content, true);  // 이메일의 내용 설정, true값을 전달하여 html 형식의 메세지를 지원
       javaMailSender.send(message);
 
     } catch (MessagingException e) {
-      // 이메일 서버에 연결할 수 없거나, 잘못된 이메일 주소를 사용하거나, 인증 오류가 발생하는 등 오류
+      // 이메일 서버에 연결할 수 없거나, 잘못된 이메일 주소를 사용하거나, 인증 오류가 발생하는 등 오류일 때
       e.printStackTrace();
     }
   }
+
 
 }

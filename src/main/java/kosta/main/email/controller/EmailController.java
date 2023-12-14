079840629bf4cd914df0d01860763208e68a538b
oyunojino <dusrn2xlayjoh@gmail.com>
2023-12-14 14:41:12 +0900

    [Feat] 주석 및 반환타입 수정

--- src/main/java/kosta/main/email/controller/EmailController.java
+++ src/main/java/kosta/main/email/controller/EmailController.java
@@ -5,26 +5,43 @@ import kosta.main.email.dto.EmailCheckDto;
 import kosta.main.email.dto.EmailSendDto;
 import kosta.main.email.service.EmailSendService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class EmailController {
   private final EmailSendService emailService;
 
+  /**
+   * 인증번호 이메일 전송
+   * @param emailSendDto
+   * @return
+   */
+
   @PostMapping("/api/mailSend")
-  public String mailSend(@RequestBody @Valid EmailSendDto emailSendDto) {
-    System.out.println("##### 인증번호 이메일 전송 #####");
-    System.out.println("이메일 :" + emailSendDto.getEmail());
-    return emailService.joinEmail(emailSendDto.getEmail());
+  public ResponseEntity<?> mailSend(@RequestBody @Valid EmailSendDto emailSendDto) {
+    log.info("이메일 :" + emailSendDto.getEmail());
+    emailService.joinEmail(emailSendDto.getEmail());
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 
+
+  /**
+   * 이메일 인증 요청
+   * @param emailCheckDto
+   * @return
+   */
   @PostMapping("/api/authNumCheck")
-  public Boolean authNumCheck(@RequestBody @Valid EmailCheckDto emailCheckDto) {
-    System.out.println("##### 이메일 인증 요청 #####");
-    System.out.println("입력값 : " + emailCheckDto.getEmailCheckNum());
-    return emailService.emailAuthNumCheck(emailCheckDto);
+  public ResponseEntity<?> authNumCheck(@RequestBody @Valid EmailCheckDto emailCheckDto) {
+    log.info("입력값 : " + emailCheckDto.getEmailCheckNum());
+    Boolean result = emailService.emailAuthNumCheck(emailCheckDto);
+    log.info("인증 여부 : " + (result ? "완료" : "오류"));
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 }

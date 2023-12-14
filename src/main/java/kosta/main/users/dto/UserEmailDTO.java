8743cc89004731251db1d9bd2544da947dd01844
oyunojino <dusrn2xlayjoh@gmail.com>
2023-12-14 17:42:17 +0900

    [Feat] builder 추가

--- src/main/java/kosta/main/users/dto/UserEmailDTO.java
+++ src/main/java/kosta/main/users/dto/UserEmailDTO.java
@@ -1,10 +1,12 @@
 package kosta.main.users.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class UserEmailDTO {
   private String email;
 }

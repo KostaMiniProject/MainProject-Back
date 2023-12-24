5f7eb4840dbc345a38e6592e05911a1ec8fe60ef
oyunojino <dusrn2xlayjoh@gmail.com>
2023-12-24 21:30:58 +0900

    [Feat] 차단당한 유저의 프로필이미지, 닉네임 반환값 추가

--- src/main/java/kosta/main/blockedusers/dto/BlockedUserResponseDTO.java
+++ src/main/java/kosta/main/blockedusers/dto/BlockedUserResponseDTO.java
@@ -18,15 +18,16 @@ public class BlockedUserResponseDTO extends Auditable {
 
     private Integer userId;
     private Integer blockingUserId;
+    private String profileImage;
+    private String name;
     public static BlockedUserResponseDTO from(User user, User blockedUser, LocalDateTime createdAt, LocalDateTime updatedAt){
         BlockedUserResponseDTO dto = new BlockedUserResponseDTO();
         dto.setUserId(user.getUserId());
         dto.setBlockingUserId(blockedUser.getUserId());
         dto.setCreatedAt(createdAt);
         dto.setUpdatedAt(updatedAt);
+        dto.setProfileImage(blockedUser.getProfileImage());
+        dto.setName(blockedUser.getName());
         return dto;
-    }
-    public static BlockedUserDTO from(User user,User blockedUser){
-        return new BlockedUserDTO(user.getUserId(), blockedUser.getUserId());
     }
 }

2aa06131d3ae6d1e4cc634e81e1f25c4f68fea5e
HeWillGoTillTheEnd <kingofunder@gmail.com>
2024-01-01 11:07:33 +0900

    Fix: 대댓글 로직 수정

--- src/main/java/kosta/main/comments/dto/CommentParentDTO.java
+++ src/main/java/kosta/main/comments/dto/CommentParentDTO.java
@@ -1,6 +1,7 @@
 package kosta.main.comments.dto;
 
 import kosta.main.comments.entity.Comment;
+import kosta.main.communityposts.entity.CommunityPost;
 import kosta.main.users.dto.response.UserCommentResponseDTO;
 import kosta.main.users.entity.User;
 import lombok.AccessLevel;
@@ -19,6 +20,12 @@ import java.util.Objects;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 public class CommentParentDTO{
+    public static final String BEFORE_SECOND = "방금전";
+    public static final String BEFORE_MINUTE = "분전";
+    public static final String BEFORE_DATE = "일전";
+    public static final String BEFORE_MONTH = "개월전";
+    public static final String BEFORE_YEAR = "년전";
+    public static final String BEFORE_HOUR = "시간전";
     private Integer commentId;
 
     private String content;
@@ -31,23 +38,46 @@ public class CommentParentDTO{
 
     private List<CommentChildDTO> children = new ArrayList<>();
 
-    public CommentParentDTO(Integer commentId, String content,Boolean isOwner,UserCommentResponseDTO profile) {
+    public CommentParentDTO(Integer commentId, String content,Boolean isOwner,UserCommentResponseDTO profile,String date) {
         this.commentId = commentId;
         this.content = content;
         this.isOwner = isOwner;
         this.profile = profile;
+        this.date = date;
     }
 
     public static CommentParentDTO from(Comment comment, Integer userId){
         if(comment.getCommentStatus() != Comment.CommentStatus.DELETED) {
-           return new CommentParentDTO(comment.getCommentId(), comment.getContent(), Objects.equals(userId, comment.getUser().getUserId()), UserCommentResponseDTO.from(comment.getUser()));
+           return new CommentParentDTO(comment.getCommentId(), comment.getContent(), Objects.equals(userId, comment.getUser().getUserId()), UserCommentResponseDTO.from(comment.getUser()), makeDate(comment.getCreatedAt()));
         } else {
-            return new CommentParentDTO(comment.getCommentId(), "삭제된 댓글입니다.", false, UserCommentResponseDTO.from(comment.getUser()));
+            return new CommentParentDTO(comment.getCommentId(), "삭제된 댓글입니다.", false, UserCommentResponseDTO.from(comment.getUser()),makeDate(comment.getCreatedAt()));
         }
     }
 
     public void addChild(CommentChildDTO childDTO){
         this.children.add(childDTO);
+    }
+
+
+    public static String makeDate(LocalDateTime createdAt) {
+        LocalDateTime now = LocalDateTime.now();
+
+        Duration duration = Duration.between(createdAt, now);
+        Period period = Period.between(createdAt.toLocalDate(), now.toLocalDate());
+
+        if (period.getYears() > 0) {
+            return period.getYears() + BEFORE_YEAR;
+        } else if (period.getMonths() > 0) {
+            return period.getMonths() + BEFORE_MONTH;
+        } else if (period.getDays() > 0) {
+            return period.getDays() + BEFORE_DATE;
+        } else if (duration.toHours() > 0) {
+            return duration.toHours() + BEFORE_HOUR;
+        } else if (duration.toMinutes() > 0) {
+            return duration.toMinutes() + BEFORE_MINUTE;
+        } else {
+            return BEFORE_SECOND;
+        }
     }
 
 
